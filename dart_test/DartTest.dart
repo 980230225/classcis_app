d1603badcbf0d0b7c344a5626f4cbad7e5299267
980230225 <a980230225@qq.com>
2021-02-18 17:55:26 +0800

    shida flutter

---
+++ dart_test/DartTest.dart
@@ -0,0 +1,24 @@
+void main(){
+  var var1=1;
+  var var2='var2';
+  print(var1);
+  print(var2);
+  print(var1.runtimeType);
+  var var3='123456';
+  print(var3);
+  print(var3.runtimeType);
+  const var4="11";
+  final var5=111;
+  final var6=1.111;
+  final var7=true;
+  print(var6.runtimeType);
+  print(var7);
+  print(var7.runtimeType);
+  var var8='''asdasd
+      a
+  ''';
+
+  print(var8);
+  var list01=['shida01','shida02'];
+  print(list01[1]);
+}
